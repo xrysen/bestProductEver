4c2e4e5943ed9d3fae88eb2d58244ff21fdde0ea
Sean Oyler <xrysen@gmail.com>
2021-05-22 18:01:49 -0700

    Initial Commit plus Navbar

---
+++ src/Components/TopNav.jsx
@@ -0,0 +1,20 @@
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+const TopNav = () => {
+  return (
+    <Navbar bg = "light" expand = "lg">
+      <Navbar.Brand href="#home">BestCo</Navbar.Brand>
+      <Navbar.Toggle aria-controls = "basic-navbar-nav" />
+      <Navbar.Collapse id = "basic-navbar-nav">
+        <Nav className = "mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#About">About</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default TopNav;
